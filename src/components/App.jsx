a3ce6c2c87d77678663f4a327342d8325a2515eb
C-L-Driver <69204245+C-L-Driver@users.noreply.github.com>
2022-06-07 22:30:44 +0100

    soliution

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [name, setName] = useState("");
+  const [nameDisplay, setNameDisplay] = useState("");
+
+  function handleChange(event) {
+    setName(event.target.value);
+  }
+
+  function handleClick() {
+    setNameDisplay(name);
+  }
+
   return (
     <div className="container">
-      <h1>Hello </h1>
-      <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <h1>Hello {nameDisplay}</h1>
+      <input
+        onChange={handleChange}
+        type="text"
+        placeholder="What's your name?"
+      />
+      <button onClick={handleClick}>Submit</button>
     </div>
   );
 }
